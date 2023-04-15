8ef830624664855b07841a318c359db5bf8b03d1
Maria Luiza <Marialuizacamilo92@gmail.com>
2023-04-14 22:33:46 -0300

    Alterações

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,25 +1,54 @@
 import React from "react";
-import{View, Text, Button, StyleSheet} from 'react-native'
+import{View, Text, Button, Image, StyleSheet} from 'react-native'
 import { ImageBackground } from "react-native-web";
 
 const Home = () => {
     return(
         <View> 
             <Text style={estilo.titulo}> Meu curriculo</Text>
+            
 
-           <Button
+            <Image  style={estilo.Image} source={require('../imagens/download.jpg')}/>
+
+           <Button  style={estilo.botaoum}
             title="Sobre"
             onPress={ () => navigation.navigate('Sobre') }
            />
+
+            <Button
+            title="Habilidade"
+            onPress={ () => navigation.navigate('Habilidade') }
+           />
+
+          <Button
+            title="Formação"
+            onPress={ () => navigation.navigate('Formação') }
+           />
+           
+
+           
         </View>
     
     );
 }
 
 const estilo = StyleSheet.create({
+
+    botaoum:{
+        fontSize:25,
+
+    },
+
     titulo: {
         textAlign:"center",
         fontSize:"50px",
+    },
+
+    Image:{
+        width:190,
+        height:190,
+        borderRadius:15,
+        left:700,
     },
 
 });
